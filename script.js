ca229d9a88c49405ff4d0a676112da1e159467df
VishalRaut2106 <rautv2005@gmail.com>
2025-08-23 00:27:05 +0530

    page 4 done

--- script.js
+++ script.js
@@ -38,13 +38,13 @@ function loadingAnimation() {
   tl.to("#loader", {
     display: "none",
   });
-  tl.from("#nav",{
-    opacity:0
-  })
-  tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
-    y:120,
-    stagger:0.2
-  })
+  tl.from("#nav", {
+    opacity: 0,
+  });
+  tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1", {
+    y: 140,
+    stagger: 0.2,
+  });
 }
 function cursorAnimation() {
   document.addEventListener("mousemove", function (dets) {
@@ -57,4 +57,4 @@ function cursorAnimation() {
   Shery.makeMagnet("#nav-part2 h4");
 }
 loadingAnimation();
-cursorAnimation()
+cursorAnimation();
