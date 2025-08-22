469f7695c7a33d374499cbad6c871f7746579649
VishalRaut2106 <rautv2005@gmail.com>
2025-08-22 12:34:24 +0530

    loader complete

---
+++ script.js
@@ -0,0 +1,26 @@
+var tl = gsap.timeline();
+tl.from(".line h1", {
+  y: 150,
+  stagger: 0.25,
+  duration: 0.6,
+  delay: 0.5,
+});
+tl.from("#line1-part1 , .line h2", {
+  opacity: 0,
+  onStart: function () {
+    var h5timer = document.querySelector("#line1-part1 h5");
+    var counter = 0;
+    setInterval(function () {
+      if (counter < 100) {
+        h5timer.innerHTML = counter++;
+      } else {
+        h5timer.innerHTML = counter;
+      }
+    }, 35);
+  },
+});
+
+tl.to("#loader", {
+  opacity: 0,
+  delay: 4,
+});
