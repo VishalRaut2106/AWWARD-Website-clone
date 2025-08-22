45dadc065969d713be633bd4a208bf13fb221119
VishalRaut2106 <rautv2005@gmail.com>
2025-08-22 22:57:28 +0530

    .

--- script.js
+++ script.js
@@ -1,55 +1,68 @@
-function loadingPage(){
+function loadingPage() {
   var tl = gsap.timeline();
-tl.from(".line h1", {
-  y: 150,
-  stagger: 0.25,
-  duration: 0.6,
-  delay: 0.5,
-});
-tl.from("#line1-part1 , .line h2", {
-  opacity: 0,
-  onStart: function () {
-    var h5timer = document.querySelector("#line1-part1 h5");
-    var counter = 0;
-    setInterval(function () {
-      if (counter < 100) {
-        h5timer.innerHTML = counter++;
-      } else {
-        h5timer.innerHTML = counter;
-      }
-    }, 35);
-  },
-});
+  tl.from(".line h1", {
+    y: 150,
+    stagger: 0.25,
+    duration: 0.6,
+    delay: 0.5,
+  });
+  tl.from("#line1-part1 , .line h2", {
+    opacity: 0,
+    onStart: function () {
+      var h5timer = document.querySelector("#line1-part1 h5");
+      var counter = 0;
+      setInterval(function () {
+        if (counter < 100) {
+          h5timer.innerHTML = counter++;
+        } else {
+          h5timer.innerHTML = counter;
+        }
+      }, 35);
+    },
+  });
 
-tl.to(".line h2", {
-  animationName:"anime",
-  opacity:1
-})
+  tl.to(".line h2", {
+    animationName: "anime",
+    opacity: 1,
+  });
 
-tl.to("#loader", {
-  opacity: 0,
-  duration: 0.2,
-  delay: 4,
-});
-tl.from("#page1", {
-  y: 1600,
-  opacity: 0,
-  delay: 0.2,
-  duration: 0.5,
-  ease: Power4,
-});
-tl.to("#loader", {
-  display: "none",
-});
-
-}
-loadingPage()
-
-document.addEventListener("mousemove",function(dets){
-  gsap.to("#crsr",{
-    left:dets.x,
-    top:dets.y
-
+  tl.to("#loader", {
+    opacity: 0,
+    duration: 0.2,
+    delay: 3,
+  });
+  tl.from("#page1", {
+    y: 1600,
+    opacity: 0,
+    delay: 0.2,
+    duration: 0.5,
+    ease: Power4,
+  });
+  tl.to("#loader", {
+    display: "none",
+  });
+  tl.from("#nav",{
+   opacity:0
   })
-  
-})
+  tl.from("#hero1 h1,#hero2 h1,#hero3 h2, #hero4 h1",{
+    y:120,
+    stagger:0.2,
+  })
+}
+
+function cursoranimation() {
+  document.addEventListener("mousemove", function (dets) {
+    gsap.to("#crsr", {
+      left: dets.x,
+      top: dets.y,
+    });
+  });
+
+  Shery.makeMagnet("#nav-part2 h4", {
+    power: 10,
+    duration: 0.4,
+  });
+}
+
+loadingPage();
+cursoranimation();
